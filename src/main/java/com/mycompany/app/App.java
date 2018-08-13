e520ef08dab7d4a9d587173a6320f9f1311bf198
sathishv81 <41816955+sathishv81@users.noreply.github.com>
2018-08-13 10:02:27 +0530

    file udpated1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!Github Integration succesfully";
+    private final String message = "Hello World!";
 
     public App() {}
 
